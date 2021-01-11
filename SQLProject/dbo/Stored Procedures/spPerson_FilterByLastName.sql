48bb8bf095c0044ba50812d5e1e0a2a663ba61e0
ABDELKARIM ELBOUROUMi <karimloco4@gmail.com>
2021-01-12 00:24:14 +0100

    Add project files.

---
+++ SQLProject/dbo/Stored Procedures/spPerson_FilterByLastName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spPerson_FilterByLastName]
+	@LastName nvarchar(50)
+AS
+begin
+
+	SELECT *
+	From dbo.Person p
+	where p.LastName = @LastName
+end
---
+++ SQLProject/dbo/Views/FullPerson.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[FullPerson]
+	AS 
+	SELECT [p].[FirstName], [p].[LastName], 
+	[a].[Id] as AddressID, [a].[StreetAddress], [a].[City], 
+	[a].[State], [a].[ZipCode]
+	From dbo.Person p
+	left join dbo.Address a ON p.[PersonId] = a.PersonId
